e6ee21e5cb3d74efd67deedeb263f055d6e136f0
CPrich905 <charlie.prichard@gmail.com>
2019-12-17 17:36:31 +0000

    updated readme - mongod still returns error message.

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/products'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/products'
 
 module.exports = { 
   port: PORT, 
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
 
 const router = require('./config/router')
 const { port, dbURI } = require('./config/environment')
 
-mongoose.connect(dbURI,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, () => console.log('database connected'))
+
+app.use(bodyParser.json())
 app.use('/api', router)
 
 app.listen(port, () => console.log(`JDLT test up and running on port ${port}`))
